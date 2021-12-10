7c4726556fd2d439ba14edaaf6e4397b40b31b97
Ryan Doherty <385683+ryanrdoherty@users.noreply.github.com>
2021-12-10 00:55:31 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#   Build Service & Dependencies
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+FROM veupathdb/alpine-dev-base:jdk-16 AS prep
+
+ARG GITHUB_USERNAME
+ARG GITHUB_TOKEN
+
+LABEL service="demo-service"
+
+WORKDIR /workspace
+RUN jlink --compress=2 --module-path /opt/jdk/jmods \
+       --add-modules java.base,java.logging,java.xml,java.desktop,java.management,java.sql,java.naming \
+       --output /jlinked \
+    && apk add --no-cache git sed findutils coreutils make npm curl gawk \
+    && git config --global advice.detachedHead false
+
+ENV DOCKER=build
+
+COPY . .
+RUN mkdir -p vendor \
+    && cp -n /jdbc/* vendor \
+    && make jar
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#   Run the service
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+FROM foxcapades/alpine-oracle:1.3
+
+LABEL service="demo-service"
+
+ENV JAVA_HOME=/opt/jdk \
+    PATH=/opt/jdk/bin:$PATH
+
+COPY --from=prep /jlinked /opt/jdk
+COPY --from=prep /workspace/build/libs/service.jar /service.jar
+
+CMD java -jar /service.jar
---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+import java.util.Properties
+import java.io.FileInputStream
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+  java
+  id("org.veupathdb.lib.gradle.container.container-utils") version "1.4.0"
+}
+
+apply(from = "dependencies.gradle.kts")
+
+// Load Props
+val buildProps = Properties()
+buildProps.load(FileInputStream(File(rootDir, "service.properties")))
+val fullPack = "${buildProps["app.package.root"]}.${buildProps["app.package.service"]}"
+
+java {
+  targetCompatibility = JavaVersion.VERSION_16
+  sourceCompatibility = JavaVersion.VERSION_16
+}
+
+containerBuild {
+}
+
+// Project settings
+group = buildProps["project.group"] ?: error("empty 1")
+version = buildProps["project.version"] ?: error("empty 2")
+
+repositories {
+  mavenCentral()
+  maven {
+    name = "GitHubPackages"
+    url  = uri("https://maven.pkg.github.com/veupathdb/maven-packages")
+    credentials {
+      username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+      password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+    }
+  }
+}
+
+tasks.jar {
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+  manifest {
+    attributes["Main-Class"] = "${fullPack}.${buildProps["app.main-class"]}"
+    attributes["Implementation-Title"] = buildProps["project.name"]
+    attributes["Implementation-Version"] = buildProps["project.version"]
+  }
+  println("Packaging Components")
+
+  from(configurations.runtimeClasspath.get().map {
+    println("  " + it.name)
+
+    if (it.isDirectory) it else zipTree(it).matching {
+      exclude { f ->
+        val name = f.name.toLowerCase()
+        (name.contains("log4j") && name.contains(".dat")) ||
+          name.endsWith(".sf") ||
+          name.endsWith(".dsa") ||
+          name.endsWith(".rsa") ||
+          name.endsWith(".md")
+      } } })
+  archiveFileName.set("service.jar")
+}
+
+tasks.register("print-package") { print(fullPack) }
+tasks.register("print-container-name") { print(buildProps["container.name"]) }
+
+tasks.withType<Test> {
+    testLogging {
+      events.addAll(listOf(TestLogEvent.FAILED,
+        TestLogEvent.SKIPPED,
+        TestLogEvent.STANDARD_OUT,
+        TestLogEvent.STANDARD_ERROR,
+        TestLogEvent.PASSED))
+
+      exceptionFormat = TestExceptionFormat.FULL
+      showExceptions = true
+      showCauses = true
+      showStackTraces = true
+      showStandardStreams = true
+      enableAssertions = true
+  }
+  ignoreFailures = true // Always try to run all tests for all modules
+}
+
+val test by tasks.getting(Test::class) {
+  // Use junit platform for unit tests
+  useJUnitPlatform()
+}
