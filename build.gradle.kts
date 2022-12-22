074a4a692701a9f9293c563ead66ac3a9021dbdb
Ryan Doherty <tech@conical.org>
2022-12-22 15:46:50 -0500

    core and common upgrades for bug fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,8 +88,8 @@ repositories {
 //
 
 // versions
-val coreLib       = "6.13.0"        // Container core lib version
-val edaCommon     = "10.0.0"        // EDA Common version
+val coreLib       = "6.13.1"        // Container core lib version
+val edaCommon     = "10.1.0"        // EDA Common version
 val fgputil       = "2.9.3-jakarta" // FgpUtil version
 
 // use local EdaCommon compiled schema if project exists, else use released version;
