26117dec12a132d6196142f0e6679f85352e0891
Dan Galdi <dmgaldi@upenn.edu>
2023-10-05 12:24:23 -0400

    Use latest EdaCommon

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ repositories {
 
 // versions
 val coreLib       = "6.16.0"         // Container core lib version
-val edaCommon     = "11.5.0"         // EDA Common version
+val edaCommon     = "11.5.3"         // EDA Common version
 val fgputil       = "2.12.9-jakarta" // FgpUtil version
 
 // ensures changing and dynamic modules are never cached
